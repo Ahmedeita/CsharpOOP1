5526b0d5e7d68d9bf26eadf3e92838bbbae9edb4
Ahmedeita <ahmedeita.dev@gmail.com>
2024-12-27 21:49:41 +0200

    Q1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,9 +1,31 @@
-﻿namespace Assignment
+﻿using System;
+
+namespace Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            #region Q1
+            /*
+             * 1.Create an enum called "WeekDays" with the days of the week (Monday to Sunday) as its members.
+             * Then, write a C# program that prints out all the days of the week using this enum */
+
+            foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+            {
+                Console.WriteLine(day);
+            }
+
+            //**********************************************************
+            //                    Another Solution
+            //**********************************************************
+
+            for (int  i = 0;  i < 7;  i++)              
+                Console.WriteLine((WeekDays)i);
+
+
+            #endregion
+
             
         }
     }
